14356d2703598c0a552aa5581a33a77f563a9b50
JosephGwara <jgwarix@gmail.com>
2021-06-11 21:59:02 +0200

    Bot form filling ability completed ,bot is able to resgresh page till elements are visible and fill in the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -72,25 +72,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(29, 22);
+            this.label1.Font = new System.Drawing.Font("a Astro Space", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 25);
+            this.label1.Size = new System.Drawing.Size(193, 18);
             this.label1.TabIndex = 1;
             this.label1.Text = "Service Booking";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(32, 97);
+            this.label2.Font = new System.Drawing.Font("a Astro Space", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(115, 105);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(185, 25);
+            this.label2.Size = new System.Drawing.Size(207, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "Personal Details";
             // 
             // name_txt
             // 
+            this.name_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.name_txt.Location = new System.Drawing.Point(37, 148);
             this.name_txt.Name = "name_txt";
             this.name_txt.Size = new System.Drawing.Size(175, 20);
@@ -106,45 +107,50 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(34, 132);
+            this.label4.Font = new System.Drawing.Font("a Astro Space", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 132);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.Size = new System.Drawing.Size(45, 11);
             this.label4.TabIndex = 7;
             this.label4.Text = "Name:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("a Astro Space", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(245, 132);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.Size = new System.Drawing.Size(70, 11);
             this.label5.TabIndex = 8;
             this.label5.Text = "Surname:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("a Astro Space", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(31, 251);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(170, 13);
+            this.label6.Size = new System.Drawing.Size(235, 11);
             this.label6.TabIndex = 14;
             this.label6.Text = "ID Number(Only Numbers Allowed)";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("a Astro Space", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(244, 193);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(46, 11);
             this.label7.TabIndex = 13;
             this.label7.Text = "Email:";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("a Astro Space", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(32, 193);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(87, 13);
+            this.label8.Size = new System.Drawing.Size(119, 11);
             this.label8.TabIndex = 12;
             this.label8.Text = "Contact Number:";
             // 
@@ -171,6 +177,9 @@
             // 
             // submit_btn
             // 
+            this.submit_btn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.submit_btn.FlatAppearance.BorderSize = 2;
+            this.submit_btn.Font = new System.Drawing.Font("a Astro Space", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.submit_btn.Location = new System.Drawing.Point(118, 361);
             this.submit_btn.Name = "submit_btn";
             this.submit_btn.Size = new System.Drawing.Size(212, 33);
@@ -182,11 +191,11 @@
             // action_lbl
             // 
             this.action_lbl.AutoSize = true;
-            this.action_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.action_lbl.Font = new System.Drawing.Font("a Astro Space", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.action_lbl.ForeColor = System.Drawing.Color.Red;
             this.action_lbl.Location = new System.Drawing.Point(32, 306);
             this.action_lbl.Name = "action_lbl";
-            this.action_lbl.Size = new System.Drawing.Size(42, 16);
+            this.action_lbl.Size = new System.Drawing.Size(54, 12);
             this.action_lbl.TabIndex = 23;
             this.action_lbl.Text = "timer";
             this.action_lbl.Visible = false;
@@ -200,6 +209,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(434, 418);
             this.Controls.Add(this.action_lbl);
             this.Controls.Add(this.submit_btn);
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ using System.Diagnostics;
 using OpenQA.Selenium.Support.UI;
 using Keys = OpenQA.Selenium.Keys;
 
+
 namespace ChurchBookingBot
 {
     public partial class Form1 : Form
@@ -24,7 +25,7 @@ namespace ChurchBookingBot
             InitializeComponent();
            
         }
-
+       
         
         private IWebDriver webDriver;
 
@@ -37,15 +38,25 @@ namespace ChurchBookingBot
 
         public void DownloadWebDriver()
         {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            webDriver = new ChromeDriver();
-
+            try { 
+                 new DriverManager().SetUpDriver(new ChromeConfig());
+            webDriver = new ChromeDriver(); }
+                catch (Exception e) {
+                MessageBox.Show("Please make sure you're connected to the internet","Error",MessageBoxButtons.OK
+                    ,MessageBoxIcon.Error);
+            
+            }
+           
         }
+      
 
         public void OpenPage() {
+            try {
+                this.webDriver.Navigate().GoToUrl(@"https://unite180.com/church_bookings/booking.php");
+            } catch (Exception e) {
+               
+            }
 
-            this.webDriver.Navigate().GoToUrl(@"https://unite180.com/church_bookings/booking.php");
-           
         }
         public void refreshPage()
         {
@@ -59,7 +70,6 @@ namespace ChurchBookingBot
 
         private void submit_btn_Click(object sender, EventArgs e)
         {
-
 
             if (serviceBooking_ComboBox.Text.Equals("*******HQ Campus*******") ||
                 serviceBooking_ComboBox.Text.Equals("*****Brooklyn Campus*****") ||
@@ -79,9 +89,10 @@ namespace ChurchBookingBot
              
             else
             {
-               
-                fillInForm();
                 timer1.Enabled = true;
+                fillInForm();
+                 
+
             }
          
         }
@@ -93,88 +104,125 @@ namespace ChurchBookingBot
             IWebElement banner = webDriver.FindElement(By.ClassName("m-portlet__body"));
             bool isDisplayed = banner.Displayed;
 
-            if (isDisplayed != true)
-            {
-                IWebElement name = webDriver.FindElement(By.Name("name"));
-                IWebElement surname = webDriver.FindElement(By.Name("surname"));
-                IWebElement number = webDriver.FindElement(By.Name("cell"));
-                IWebElement email = webDriver.FindElement(By.Name("email"));
-                IWebElement identity = webDriver.FindElement(By.Name("id_number"));
-                IWebElement question1 = webDriver.FindElement(By.Name("question1"));
-                IWebElement question2 = webDriver.FindElement(By.Name("question2"));
-                IWebElement question3 = webDriver.FindElement(By.Name("question3"));
+           
+                if (isDisplayed != true)
+                {
+                    IWebElement name = webDriver.FindElement(By.Name("name"));
+                    IWebElement surname = webDriver.FindElement(By.Name("surname"));
+                    IWebElement number = webDriver.FindElement(By.Name("cell"));
+                    IWebElement email = webDriver.FindElement(By.Name("email"));
+                    IWebElement identity = webDriver.FindElement(By.Name("id_number"));
+                    IWebElement question1 = webDriver.FindElement(By.Name("question1"));
+                    IWebElement question2 = webDriver.FindElement(By.Name("question2"));
+                    IWebElement question3 = webDriver.FindElement(By.Name("question3"));
 
 
-                name.SendKeys(name_txt.Text);
-                surname.SendKeys(surname_txt.Text);
-                number.SendKeys(contact_txt.Text);
-                email.SendKeys(email.Text);
-                identity.SendKeys(ID_txt.Text);
+                    name.SendKeys(name_txt.Text);
+                    surname.SendKeys(surname_txt.Text);
+                    number.SendKeys(contact_txt.Text);
+                    email.SendKeys(email_txt.Text);
+                    identity.SendKeys(ID_txt.Text);
 
-                name.SendKeys(Keys.Return);
-                surname.SendKeys(Keys.Return);
-                number.SendKeys(Keys.Return);
-                email.SendKeys(Keys.Return);
-                identity.SendKeys(Keys.Return);
+                    name.SendKeys(Keys.Return);
+                    surname.SendKeys(Keys.Return);
+                    number.SendKeys(Keys.Return);
+                    email.SendKeys(Keys.Return);
+                    identity.SendKeys(Keys.Return);
 
-                SelectElement selectQ1 = new SelectElement(question1);
-                selectQ1.SelectByValue("No");
-                SelectElement selectQ2 = new SelectElement(question2);
-                selectQ2.SelectByValue("No");
-                SelectElement selectQ3 = new SelectElement(question3);
-                selectQ3.SelectByValue("No");
+                    SelectElement selectQ1 = new SelectElement(question1);
+                    selectQ1.SelectByValue("No");
+                    SelectElement selectQ2 = new SelectElement(question2);
+                    selectQ2.SelectByValue("No");
+                    SelectElement selectQ3 = new SelectElement(question3);
+                    selectQ3.SelectByValue("No");
 
-                IWebElement submit = webDriver.FindElement(By.Name("question3"));
-                submit.Click();
+
+                string num1 = webDriver.FindElement(By.Id("num1")).GetAttribute("value");
+                string num2 = webDriver.FindElement(By.Id("num2")).GetAttribute("value");
+                int numOne = Convert.ToInt32(num1);
+                int numTwo = Convert.ToInt32(num2);
+                int result = numOne + numTwo;
+
+                IWebElement sum = webDriver.FindElement(By.Id("sumOff"));
+                sum.SendKeys(result.ToString());
+                sum.SendKeys(Keys.Return);
+
+
+                IWebElement submit = webDriver.FindElement(By.Id("dis"));
+                bool displayed = submit.Displayed;
+
+                if (displayed == true) {
+                    submit.Click();
+                    timer1.Enabled = false;
+                    
+                    
+                }
+                else
+                {
+                    //Do nothing
+                }
+               
+
             }
+
+            
         }
         public void ServiceBooking()
         {
             string selection = serviceBooking_ComboBox.Text;
 
-            IWebElement banner = webDriver.FindElement(By.ClassName("m-portlet__body"));
-            bool isDisplayed = banner.Displayed;
 
-            if (isDisplayed != true) {
+            try
+            {
+                IWebElement banner = webDriver.FindElement(By.ClassName("m-portlet__body"));
+                bool isDisplayed = banner.Displayed;
 
-                IWebElement service = webDriver.FindElement(By.Name("service"));
 
-                switch (selection)
+                if (isDisplayed != true)
                 {
-                    case "1.Morning Service(08:55)":
-                        SelectElement s1 = new SelectElement(service); s1.SelectByValue("hq_1");
-                        break;
-                    case "2.Evening Service(16:55)":
-                        SelectElement s2 = new SelectElement(service); s2.SelectByValue("hq_2");
-                        break;
-                    case "3.Prayer Night(21:30)":
-                        SelectElement s3 = new SelectElement(service); s3.SelectByValue("HQ");
-                        break;
-                    case "4.Morning Service(09:00)":
-                        SelectElement s4 = new SelectElement(service); s4.SelectByValue("bn_1");
-                        break;
-                    case "5.Evening Service(17:00)":
-                        SelectElement s5 = new SelectElement(service); s5.SelectByValue("bn_2");
-                        break;
-                    case "6.First Service(16:00)":
-                        SelectElement s6 = new SelectElement(service); s6.SelectByValue("ph_1");
-                        break;
-                    case "7.Second Service(18:00)":
-                        SelectElement s7 = new SelectElement(service); s7.SelectByValue("ph_2");
-                        break;
-                    case "8.Prayer Night(21:30)":
-                        SelectElement s8 = new SelectElement(service); s8.SelectByValue("Potch");
-                        break;
-}
+
+                    IWebElement service = webDriver.FindElement(By.Name("service"));
+
+                    switch (selection)
+                    {
+                        case "1.Morning Service(08:55)":
+                            SelectElement s1 = new SelectElement(service); s1.SelectByValue("hq_1");
+                            break;
+                        case "2.Evening Service(16:55)":
+                            SelectElement s2 = new SelectElement(service); s2.SelectByValue("hq_2");
+                            break;
+                        case "3.Prayer Night(21:30)":
+                            SelectElement s3 = new SelectElement(service); s3.SelectByValue("HQ");
+                            break;
+                        case "4.Morning Service(09:00)":
+                            SelectElement s4 = new SelectElement(service); s4.SelectByValue("bn_1");
+                            break;
+                        case "5.Evening Service(17:00)":
+                            SelectElement s5 = new SelectElement(service); s5.SelectByValue("bn_2");
+                            break;
+                        case "6.First Service(16:00)":
+                            SelectElement s6 = new SelectElement(service); s6.SelectByValue("ph_1");
+                            break;
+                        case "7.Second Service(18:00)":
+                            SelectElement s7 = new SelectElement(service); s7.SelectByValue("ph_2");
+                            break;
+                        case "8.Prayer Night(21:30)":
+                            SelectElement s8 = new SelectElement(service); s8.SelectByValue("Potch");
+                            break;
+                    }
+
+                }
             }
-
-
+            catch (Exception e) { }
 
         }
+        
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             refreshPage();
+            fillInForm();
+
         }
     }
 }
